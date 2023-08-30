e0cf9cb7847f9e4d185b6a43c0c3375abc4bfdfd
Romashka <rroma13579@gmail.com>
2023-08-30 19:31:47 +0200

    added code from js to ts, feels like ive done it poorly, but its my first attempt

--- my-animations/src/app/four-cards/four-cards.component.ts
+++ my-animations/src/app/four-cards/four-cards.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BIGCARDS, SMALLCARDS } from '../four-cards-list';
 
 @Component({
@@ -6,7 +6,43 @@ import { BIGCARDS, SMALLCARDS } from '../four-cards-list';
   templateUrl: './four-cards.component.html',
   styleUrls: ['./four-cards.component.css']
 })
-export class FourCardsComponent {
-    bigCards = BIGCARDS;
-    smallCards = SMALLCARDS;
+export class FourCardsComponent implements OnInit {
+  bigCards = BIGCARDS;
+  smallCards = SMALLCARDS;
+
+  ngOnInit(): void {
+    var fourCards = document.querySelectorAll('[data-status]');
+    var buttonN = document.querySelector('.next');
+    var buttonB = document.querySelector('.back');
+
+    var i = 0;
+
+    fourCards.forEach(card => {
+      if (card instanceof HTMLElement) {
+        if (card.dataset.status == "active") {
+          if (card.dataset.index)
+            i = parseInt(card.dataset.index);
+        }
+        console.log(i);
+      }
+    });
+
+    (buttonN as HTMLInputElement).addEventListener('click', () => {
+      (fourCards[i] as HTMLInputElement).dataset.status = "after";
+      i = i + 1 <= fourCards.length - 1 ? i + 1 : 0;
+      (fourCards[i] as HTMLInputElement).dataset.status = "pre-before";
+      setTimeout(() => {
+        (fourCards[i] as HTMLInputElement).dataset.status = "active";
+      });
+    });
+    (buttonB as HTMLInputElement).addEventListener('click', () => {
+      (fourCards[i] as HTMLInputElement).dataset.status = "before";
+      i = i - 1 >= 0 ? i - 1 : fourCards.length - 1;
+      (fourCards[i] as HTMLInputElement).dataset.status = "pre-after";
+      setTimeout(() => {
+        (fourCards[i] as HTMLInputElement).dataset.status = "active";
+      });
+    });
+
+  }
 }

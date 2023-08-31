9f03c4e1a2f45054296cf97ce868ef2d1d554a75
Romashka <rroma13579@gmail.com>
2023-08-31 16:59:10 +0200

    cards chage size

--- my-animations/src/app/four-squares-to-screen/four-squares-to-screen.component.ts
+++ my-animations/src/app/four-squares-to-screen/four-squares-to-screen.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-four-squares-to-screen',
   templateUrl: './four-squares-to-screen.component.html',
   styleUrls: ['./four-squares-to-screen.component.css']
 })
-export class FourSquaresToScreenComponent {
+export class FourSquaresToScreenComponent implements OnInit {
+  ngOnInit(): void {
+    var squares = document.querySelectorAll('.square');
 
+    squares.forEach(square => {
+      (square as HTMLInputElement).addEventListener('click', () => {
+        (square as HTMLInputElement).dataset.status = "opened";
+      });
+    });
+  }
 }
